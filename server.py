0c29711122a83358da63cceeab1b188b7f1ac43a
Leandro M. Regolf <leandroregolf@yahoo.com.ar>
2022-01-16 16:17:31 -0300

    Primera estructura de archivos + Referencias graficas

---
+++ server.py
@@ -0,0 +1,17 @@
+# --------------------------------------------------
+# Proyecto final 2021
+#
+# Programación II
+# Laboratorio de computación II
+# TUP - UTN FRBB
+#
+# Leandro M. Regolf & Agustín Ascolani
+# --------------------------------------------------
+#
+# Crear una plataforma web para mantener una lista de
+# películas recomendadas por parte de los usuarios
+# --------------------------------------------------
+
+
+
+
