c68ce0a9e266a894d869ac5fe3a769111df2c10a
Leandro M. Regolf <leandroregolf@yahoo.com.ar>
2022-01-25 23:50:13 -0300

    Mas datas JSON externas

--- server.py
+++ server.py
@@ -29,41 +29,25 @@ servidor_API = Flask(servidor_nombre)
 
 # Data
 ruta_actual = os.path.dirname(__file__)
+ruta_data = ruta_actual + "/__data/"
 
-with open(os.path.join(ruta_actual, '__server_data/usuarios.json'), 'r', encoding="utf-8") as data_JSON:
+with open(os.path.join(ruta_data, 'usuarios.json'), 'r', encoding="utf-8") as data_JSON:
     usuarios = json.load(data_JSON)
 
-with open(os.path.join(ruta_actual, '__server_data/data.json'), 'r', encoding="utf-8") as data_JSON:
+with open(os.path.join(ruta_data, 'peliculas.json'), 'r', encoding="utf-8") as data_JSON:
     peliculas = json.load(data_JSON)
 
+with open(os.path.join(ruta_data, 'directores.json'), 'r', encoding="utf-8") as data_JSON:
+    directores = json.load(data_JSON)
 
-# peliculasNO = [
-#     {
-#         "id": 1,
-#         "nombre": "ROBOCOP"
-#     },
-#     {
-#         "id": 2,
-#         "nombre": "Terminator"
-#     },
-#     {
-#         "id": 3,
-#         "nombre": "Volver al futuro"
-#     }
-# ]
-#
-usuarios = [
-    {
-        "id": 1,
-        "nombre": "Leandro",
-        "clave": "pass1"
-    },
-    {
-        "id": 2,
-        "nombre": "Agustín",
-        "clave": "pass2"
-    }
-]
+with open(os.path.join(ruta_data, 'generos.json'), 'r', encoding="utf-8") as data_JSON:
+    generos = json.load(data_JSON)
+
+with open(os.path.join(ruta_data, 'comentarios.json'), 'r', encoding="utf-8") as data_JSON:
+    comentarios = json.load(data_JSON)
+
+
+
 
 
 
@@ -72,6 +56,7 @@ usuarios = [
 @servidor_API.route("/", methods=["GET"])
 def default():
     return servidor_nombre, HTTPStatus.OK
+
 
 @servidor_API.route("/test", methods=["GET"])
 @servidor_API.route("/test/", methods=["GET"])
