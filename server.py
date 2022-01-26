2e12a1c17beb68b530f3cafcb15813239b09a6f2
Leandro M. Regolf <leandroregolf@yahoo.com.ar>
2022-01-25 23:26:27 -0300

    Lectura de JSON de archivos externos

--- server.py
+++ server.py
@@ -13,40 +13,83 @@
 # --------------------------------------------------
 
 
-from flask import Flask
-from flask import jsonify
+# Includes
+import os                       # Lectura y escritura de archivos
+import json                     # Manipular JSON
+from flask import Flask         # Servidor FLASK
+from flask import jsonify       # Manipular JSON
+from http import HTTPStatus     # Códigos HTTP legibles
 
 
-server_name = "Movies API server [ PrgLabII 2022 ]"
+# Configuración
+servidor_nombre = "Movies API server [ PrgLabII 2022 ]"
 
-
-movies_server = Flask(server_name)
+servidor_API = Flask(servidor_nombre)
 
 
 # Data
-peliculas = [
+ruta_actual = os.path.dirname(__file__)
+
+with open(os.path.join(ruta_actual, '__server_data/usuarios.json'), 'r', encoding="utf-8") as data_JSON:
+    usuarios = json.load(data_JSON)
+
+with open(os.path.join(ruta_actual, '__server_data/data.json'), 'r', encoding="utf-8") as data_JSON:
+    peliculas = json.load(data_JSON)
+
+
+# peliculasNO = [
+#     {
+#         "id": 1,
+#         "nombre": "ROBOCOP"
+#     },
+#     {
+#         "id": 2,
+#         "nombre": "Terminator"
+#     },
+#     {
+#         "id": 3,
+#         "nombre": "Volver al futuro"
+#     }
+# ]
+#
+usuarios = [
     {
         "id": 1,
-        "nombre": "ROBOCOP"
+        "nombre": "Leandro",
+        "clave": "pass1"
     },
     {
         "id": 2,
-        "nombre": "Terminator"
-    },
-    {
-        "id": 3,
-        "nombre": "Volver al futuro"
+        "nombre": "Agustín",
+        "clave": "pass2"
     }
 ]
 
 
-@movies_server.route("/", methods=["GET"])
+
+# - - Principal - -
+
+@servidor_API.route("/", methods=["GET"])
 def default():
-    return server_name
+    return servidor_nombre, HTTPStatus.OK
 
-@movies_server.route("/test", methods=["GET"])
+@servidor_API.route("/test", methods=["GET"])
+@servidor_API.route("/test/", methods=["GET"])
 def funcion():
-    return jsonify(peliculas), 200
+    return jsonify(peliculas), HTTPStatus.OK
 
 
-movies_server.run()
+
+@servidor_API.route("/usuarios", methods=["GET"])
+@servidor_API.route("/usuarios/", methods=["GET"])
+def funcionusuarios():
+    return jsonify(usuarios), HTTPStatus.OK
+
+
+@servidor_API.route("/peliculas", methods=["GET"])
+@servidor_API.route("/peliculas/", methods=["GET"])
+def peliculasr():
+    return jsonify(peliculas), HTTPStatus.OK
+
+
+servidor_API.run()
