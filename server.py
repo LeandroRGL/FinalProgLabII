368290700fd72a0b492585684242117070d08cd4
Leandro M. Regolf <leandroregolf@yahoo.com.ar>
2022-01-29 15:17:42 -0300

    end-points películas / comentarios / directores + nomencalturas en funciones

--- server.py
+++ server.py
@@ -58,23 +58,88 @@ def default():
     return servidor_nombre, HTTPStatus.OK
 
 
-@servidor_API.route("/test", methods=["GET"])
-@servidor_API.route("/test/", methods=["GET"])
-def funcion():
-    return jsonify(peliculas), HTTPStatus.OK
-
-
-
-@servidor_API.route("/usuarios", methods=["GET"])
-@servidor_API.route("/usuarios/", methods=["GET"])
-def funcionusuarios():
-    return jsonify(usuarios), HTTPStatus.OK
-
-
+# -- Películas
 @servidor_API.route("/peliculas", methods=["GET"])
 @servidor_API.route("/peliculas/", methods=["GET"])
-def peliculasr():
-    return jsonify(peliculas), HTTPStatus.OK
+def peliculas_devolver_todas():
+    response = jsonify(peliculas)
+    return response, HTTPStatus.OK
+
+@servidor_API.route("/peliculas/<clnt_id>", methods=["GET"])
+def peliculas_devolver_una(clnt_id):
+    if clnt_id.isnumeric():
+        id = int(clnt_id)
+
+        for pelicula in peliculas:
+            if pelicula["id"] == id:
+                return jsonify(pelicula["título"]), HTTPStatus.OK
+        return jsonify("[nfo] - <" + str(id) + "> no existe"), HTTPStatus.NOT_FOUND
+    else:
+        return jsonify("[err] - <id> debe ser entero"), HTTPStatus.BAD_REQUEST
+
+
+
+
+
+
+# -- Comentarios
+@servidor_API.route("/comentarios", methods=["GET"])
+@servidor_API.route("/comentarios/", methods=["GET"])
+def comentarios_devolver_todos():
+    return jsonify(comentarios), HTTPStatus.OK
+
+@servidor_API.route("/peliculas/<clnt_id>/comentarios", methods=["GET"])
+@servidor_API.route("/peliculas/<clnt_id>/comentarios/", methods=["GET"])
+def comentarios_devolver_por_pelicula(clnt_id):
+    if clnt_id.isnumeric():
+        id = int(clnt_id)
+
+        for comentario in comentarios:
+            if comentario["película_id"] == id:
+                return jsonify(comentario["opinión"]), HTTPStatus.OK
+        return jsonify("[nfo] - <" + str(id) + "> no existe"), HTTPStatus.NOT_FOUND
+    else:
+        return jsonify("[err] - <id> debe ser entero"), HTTPStatus.BAD_REQUEST
+
+@servidor_API.route("/usuarios/<clnt_id>/comentarios", methods=["GET"])
+@servidor_API.route("/usuarios/<clnt_id>/comentarios/", methods=["GET"])
+def comentarios_devolver_por_usuario(clnt_id):
+    if clnt_id.isnumeric():
+        id = int(clnt_id)
+
+        for comentario in comentarios:
+            if comentario["usuario_id"] == id:
+                return jsonify(comentario["opinión"]), HTTPStatus.OK
+        return jsonify("[nfo] - <" + str(id) + "> no existe"), HTTPStatus.NOT_FOUND
+    else:
+        return jsonify("[err] - <id> debe ser entero"), HTTPStatus.BAD_REQUEST
+
+
+# -- Directores
+@servidor_API.route("/directores", methods=["GET"])
+@servidor_API.route("/directores/", methods=["GET"])
+def directores_devolver_todos():
+    return jsonify(directores), HTTPStatus.OK
+
+# @servidor_API.route("/test", methods=["GET"])
+# @servidor_API.route("/test/", methods=["GET"])
+# def funcion():
+#     return jsonify(peliculas), HTTPStatus.OK
+
+
+
+# @servidor_API.route("/usuarios", methods=["GET"])
+# @servidor_API.route("/usuarios/", methods=["GET"])
+# def funcionusuarios():
+#     return jsonify(usuarios), HTTPStatus.OK
+
+
+# -- Géneros
+@servidor_API.route("/generos", methods=["GET"])
+@servidor_API.route("/generos/", methods=["GET"])
+def generos_devolver_todos():
+    return jsonify(generos), HTTPStatus.OK
+
 
 
 servidor_API.run()
